4e844b6ecec98b1eaea81d7040f868eedc1321f0
faladetimilehin <faladetimilehin@yahoo.com>
2020-04-14 16:45:53 +0100

    navbar and landing

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -2,7 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import {Button} from 'components/utility'
-
+import './landing.css'
+import img7 from '../img/car07.jpg'
 const ParentDiv = styled.div`
     background: #3384f0;
     width: 100%;
@@ -15,8 +16,44 @@ const ParentDiv = styled.div`
 const LandingPage = ({})=> {
     return (
         <ParentDiv>
-            <h1> APP PAGE </h1>
-            content here
+             <div class="d-md-flex h-md-100 align-items-center">
+                <div class="col-md-6 p-0 bg-indigo h-md-100">
+                    <div class="text-white d-md-flex align-items-center h-100 p-5 text-center justify-content-center">
+                        <div class="logoarea pt-5 pb-5">
+                        <p>
+					<i class="fa fa-anchor fa-3x"></i>
+				</p>
+				<h1 class="mb-0 mt-3 display-4"> Smart Wash</h1>
+                
+				<h5 class="mb-4 font-weight-light">The fast, affordable way to wash your car.</h5>
+				<a target="_blank" class="btn btn-outline-light btn-lg btn-round" href="/login">Get Started</a>
+				
+
+                    
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 p-0 bg-white h-md-100 loginarea">
+                    <div class="d-md-flex align-items-center h-md-100 p-5 justify-content-center">
+                    <form class="border rounded p-5">
+                    
+                    <h3 class="mb-4 text-center">Sign In</h3>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail" required=""></input>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required=""></input>
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+                        <label class="form-check-label small text-muted" for="exampleCheck1">Remember me</label>
+                    </div>
+                    <button type="submit" class="btn btn-success btn-round btn-block shadow-sm">Sign in</button>
+                    <small class="d-block mt-4 text-center"><a class="text-gray" href="#">Forgot your password?</a></small>
+                </form>
+    </div>
+                </div>
+            </div>
         </ParentDiv>
     )
 }
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import styled, { css } from 'styled-components'
 
+
 const NavDIv = styled.div`
     width: 100%;
     background: black;
@@ -17,12 +18,32 @@ const NavDIv = styled.div`
 const NavBar = ({auth: { isAuthenticated, isAdmin }}) => {
     return (
         <NavDIv isAuthenticated = {isAuthenticated}>
-            <div className="d-flex justify-content-between align-content-center">
-                <i className="fas fa-car text-light"></i>
-                <div>
-                    <h1 className="text-light"> Nav bar here </h1>
-                </div>
-            </div>
+           <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+            <header>
+                <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                    <a class="navbar-brand" href="#">Smart Wash
+                    <svg class="bi bi-building" width="1.5em" color="white" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M15.285.089A.5.5 0 0115.5.5v15a.5.5 0 01-.5.5h-3a.5.5 0 01-.5-.5V14h-1v1.5a.5.5 0 01-.5.5H1a.5.5 0 01-.5-.5v-6a.5.5 0 01.418-.493l5.582-.93V3.5a.5.5 0 01.324-.468l8-3a.5.5 0 01.46.057zM7.5 3.846V8.5a.5.5 0 01-.418.493l-5.582.93V15h8v-1.5a.5.5 0 01.5-.5h2a.5.5 0 01.5.5V15h2V1.222l-7 2.624z" clip-rule="evenodd" />
+                        <path fill-rule="evenodd" d="M6.5 15.5v-7h1v7h-1z" clip-rule="evenodd" />
+                        <path d="M2.5 11h1v1h-1v-1zm2 0h1v1h-1v-1zm-2 2h1v1h-1v-1zm2 0h1v1h-1v-1zm6-10h1v1h-1V3zm2 0h1v1h-1V3zm-4 2h1v1h-1V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm-2 2h1v1h-1V7zm2 0h1v1h-1V7zm-4 0h1v1h-1V7zm0 2h1v1h-1V9zm2 0h1v1h-1V9zm2 0h1v1h-1V9zm-4 2h1v1h-1v-1zm2 0h1v1h-1v-1zm2 0h1v1h-1v-1z" />
+                    </svg>
+                    </a>
+                    
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav mr-auto">
+                        </ul>
+                        <a className="p-2 text-white" to="/home">Home</a>
+                        <a className="p-2 text-white" to="/about">About</a>
+                        <a className="p-2 text-white" to="/services">Services</a>
+                        <a className="p-2 text-white" to="/contact">Contact</a>
+                        <a class="btn btn-outline-primary" href="/login">Login</a>
+                    </div>
+                </nav>
+            </header>
+        </div>
         </NavDIv>
     )
 };
